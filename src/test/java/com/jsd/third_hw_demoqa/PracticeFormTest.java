a04b71eb849b2412882bf5624d22db975d037227
Dmitry F <dimfroloff@gmail.com>
2023-02-18 22:43:04 +0300

    Automation practice form test

--- src/test/java/com/jsd/third_hw_demoqa/PracticeFormTest.java
+++ src/test/java/com/jsd/third_hw_demoqa/PracticeFormTest.java
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
-
-    @Test
-    void initialTest() {
-        System.out.println("Initial test");
-    }
 
     @BeforeAll
     static void beforeAll() {
@@ -22,14 +21,46 @@ public class PracticeFormTest {
     @Test
     void fillFormTest() {
         open("https://demoqa.com/automation-practice-form");
-//        executeJavaScript("document.body.style.zoom='80%'");
-//        $("[id=firstName]").setValue("F");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 //        id = #, class = .
         $("#firstName").setValue("F");
         $("#lastName").setValue("D");
+        $("#userEmail").setValue("test@test.com");
         $(By.cssSelector("label[for='gender-radio-1']")).click();
         $("#userNumber").setValue("0123456789");
-//        $(By.id("submit")).hover();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("9");
+        $(".react-datepicker__year-select").selectOptionByValue("1990");
+        $(By.cssSelector("div.react-datepicker__day[aria-label='Choose Wednesday, October 31st, 1990']")).click();
+        $("#subjectsInput").setValue("M").pressEnter();
+        $(By.cssSelector("label[for='hobbies-checkbox-1']")).click();
+        $(By.cssSelector("label[for='hobbies-checkbox-2']")).click();
+        $(By.cssSelector("label[for='hobbies-checkbox-3']")).click();
+
+        File fileToUpload = new File("photo_2023-02-16_19-03-31.jpg");
+        $("#uploadPicture").uploadFile(fileToUpload);
+
+        $("#currentAddress").setValue("planet Earth");
+        $("#state").click();
+        $("#react-select-3-option-0").click();
+
+        $("#city").click();
+        $("#react-select-4-option-0").click();
+
         $("#submit").click();
+
+        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
+        $("table tbody tr:nth-child(1) td:nth-child(2)").shouldHave(exactText("F D"));
+        $("table tbody tr:nth-child(2) td:nth-child(2)").shouldHave(exactText("test@test.com"));
+        $("table tbody tr:nth-child(3) td:nth-child(2)").shouldHave(exactText("Male"));
+        $("table tbody tr:nth-child(4) td:nth-child(2)").shouldHave(exactText("0123456789"));
+        $("table tbody tr:nth-child(5) td:nth-child(2)").shouldHave(exactText("31 October,1990"));
+        $("table tbody tr:nth-child(6) td:nth-child(2)").shouldHave(exactText("Maths"));
+        $("table tbody tr:nth-child(7) td:nth-child(2)").shouldHave(exactText("Sports, Reading, Music"));
+        $("table tbody tr:nth-child(8) td:nth-child(2)").shouldHave(exactText("photo_2023-02-16_19-03-31.jpg"));
+        $("table tbody tr:nth-child(9) td:nth-child(2)").shouldHave(exactText("planet Earth"));
+        $("table tbody tr:nth-child(10) td:nth-child(2)").shouldHave(exactText("NCR Delhi"));
+
     }
 }
