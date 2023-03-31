b2254b5706cd6c4d3c24eb9ad8a0ec6201ab1c9f
Dmitry F <dimfroloff@gmail.com>
2023-03-31 22:26:37 +0300

    Jenkins params

--- src/test/java/com/jsd/registration_page_jenkins/TestBaseExtended.java
+++ src/test/java/com/jsd/registration_page_jenkins/TestBaseExtended.java
@@ -21,8 +21,7 @@ public class TestBaseExtended {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = System.getProperty("base_url", "https://demoqa.com" );
-        Configuration.remote = "https://user1:1234@"
-                               + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browser_version", "100.0");
         Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
