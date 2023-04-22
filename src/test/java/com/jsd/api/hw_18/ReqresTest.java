fe15965a2793e02e81bcc1325d69f24de3902b94
Dmitry F <dimfroloff@gmail.com>
2023-04-22 21:30:37 +0300

    API rest assured hw

--- src/test/java/com/jsd/api/hw_18/ReqresTest.java
+++ src/test/java/com/jsd/api/hw_18/ReqresTest.java
@@ -1,7 +1,91 @@
 package com.jsd.api.hw_18;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+@Tag("api")
 public class ReqresTest {
 
+    @Test
+    void dataArraySizeTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data", hasSize(6));
+    }
+
+    @Test
+    void updateUserTest() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void deleteUserTest() {
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+    @Test
+    void testDataArrayContainsColorsTest() {
+        List<String> expectedColors = Arrays.asList("cerulean", "fuchsia rose", "true red",
+                "aqua sky", "tigerlily", "blue turquoise");
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/unknown")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.name", hasItems(expectedColors.toArray()));
+    }
+
+    @Test
+    void loginWithIncorrectEmailTest() {
+        String body = "{ \"email\": \"incorrect@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("user not found"));
+    }
 }
